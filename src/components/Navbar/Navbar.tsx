96897a6e3804a6ec721d8814b996e248e1dd73b3
ricardobizerra <ricardo.bizerra19@gmail.com>
2023-12-01 08:34:28 -0300

    chore: use app router & redesign website

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+function Navbar () {
+    return (
+        <div>
+            <p>
+                Apresentando o &quot;HairBoost&quot; - o produto revolucionário projetado para acelerar a calvície. Com sua fórmula avançada e ingredientes naturais, o HairBoost estimula o processo de queda de cabelo, proporcionando uma experiência mais rápida e eficiente para quem deseja lidar com a calvície. Diga adeus aos cabelos indesejados e olá a uma aparência renovada. Experimente o HairBoost hoje mesmo e veja os resultados impressionantes!
+            </p>
+        </div>
+    )
+}
+
+export default Navbar;
--- src/components/index.ts
+++
@@ -1,9 +0,0 @@
-import RBLogo from "./RBLogo";
-import RedirectButton from "./RedirectButton";
-import SocialMediaMenu from "./SocialMediaMenu";
-
-export {
-	RBLogo,
-	RedirectButton,
-	SocialMediaMenu,
-};
