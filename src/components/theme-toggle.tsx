4f860befb3c126faf98a393a7471cf52a6261f5e
ricardobizerra <ricardo.bizerra19@gmail.com>
2025-09-22 01:03:22 -0300

    fix: update theme toggle importing from icons component

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 import { Button } from '@/components/ui/button';
 import {
@@ -9,6 +8,7 @@ import {
   DropdownMenuCheckboxItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import { Icons } from '@/components/icons';
 
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
@@ -17,8 +17,8 @@ export function ThemeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <Icons.sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <Icons.moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
           <span className="sr-only">Mudar tema</span>
         </Button>
       </DropdownMenuTrigger>
