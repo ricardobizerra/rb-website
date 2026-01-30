069a2704a2346733aeeff4576c11617e1d3dd992
ricardobizerra <ricardo.bizerra19@gmail.com>
2026-01-30 08:50:06 -0300

    feat: svg, path, g fill color enforcement on button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,11 +9,11 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground [a]:hover:bg-primary/80",
+        default: "bg-primary text-primary-foreground [a]:hover:bg-primary/80 [&_svg:not(.lucide),&_svg:not(.lucide)_path,&_svg:not(.lucide)_g]:fill-background! dark:[&_svg:not(.lucide),&_svg:not(.lucide)_path,&_svg:not(.lucide)_g]:fill-foreground!",
         outline:
           "border-border bg-background hover:bg-muted hover:text-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 aria-expanded:bg-muted aria-expanded:text-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80 aria-expanded:bg-secondary aria-expanded:text-secondary-foreground",
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80 aria-expanded:bg-secondary aria-expanded:text-secondary-foreground [&_svg:not(.lucide),&_svg:not(.lucide)_path,&_svg:not(.lucide)_g]:fill-background! dark:[&_svg:not(.lucide),&_svg:not(.lucide)_path,&_svg:not(.lucide)_g]:fill-foreground!",
         ghost:
           "hover:bg-muted hover:text-foreground dark:hover:bg-muted/50 aria-expanded:bg-muted aria-expanded:text-foreground",
         destructive:
